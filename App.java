c6b4003bc83d6cf25fc5041c1a186d2cb03f296b
viniwittlerizipaynow <vinicius.steiner@izipaynow.com>
2024-10-11 12:27:55 -0300

    scanner app

--- App.java
+++ App.java
@@ -7,8 +7,27 @@ import dados.Usuario;
 public class App {
 
     private static List<Usuario> usuarios = new ArrayList<>();
+    private static Usuario usuarioAtual;
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcao = -1;
 
+        while (opcao != 0 && usuarioAtual == null) {
+            //exibirMenuInicial();
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    //selecionarUsuario();
+                    break;
+                case 0:
+                    System.out.println("Saindo do sistema.");
+                    System.exit(0);
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        }
     }
 }
