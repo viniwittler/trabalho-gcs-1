79f2cfa6de35b69276a1fa3d1cc91ea6b5ec3f62
viniwittler <viniwittler@hotmail.com>
2024-10-11 12:46:03 -0300

    melhorar o app

--- App.java
+++ App.java
@@ -3,11 +3,14 @@ import java.util.List;
 import java.util.Scanner;
 
 import dados.Administrador;
+import dados.Departamento;
+import dados.Funcionario;
 import dados.Usuario;
 
 public class App {
 
     private static List<Usuario> usuarios = new ArrayList<>();
+    private static List<Departamento> departamentos = new ArrayList<>();
     private static Usuario usuarioAtual;
 
     public static void main(String[] args) {
@@ -35,7 +38,15 @@ public class App {
 
     private static void inicializarDados() {
 
+        departamentos.add(new Departamento("Financeiro", 5000));
+        departamentos.add(new Departamento("RH", 3000));
+        departamentos.add(new Departamento("Engenharia", 8000));
+        departamentos.add(new Departamento("Manutenção", 4000));
+        departamentos.add(new Departamento("Compras", 6000));
+
         usuarios.add(new Administrador("admin01", "Carlos Lacerda"));
+        usuarios.add(new Funcionario("func01", "João Silva", departamentos.get(0)));
+        usuarios.add(new Funcionario("func02", "Maria Souza", departamentos.get(1)));
 
         usuarioAtual = null;
     }
