11973cf4d357e602b5ff6e25404d15ae94d98689
viniwittler <viniwittler@hotmail.com>
2024-10-11 09:11:48 -0300

    include usuario no app

--- App.java
+++ App.java
@@ -1,6 +1,14 @@
-public class App {
-    public static void main(String[] args) {
-      
-    }
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+import dados.Usuario;
+
+public class App {
+
+    private static List<Usuario> usuarios = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+    }
 }
